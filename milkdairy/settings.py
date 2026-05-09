50c8a2e7b237ca442f02ff8aa95a09852c91119f
Adhil Najeeb <adhilnajeeb469@gmail.com>
2026-05-09 13:59:06 +0530

    Procfile change

--- milkdairy/settings.py
+++ milkdairy/settings.py
@@ -69,6 +69,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
+        ssl_require=True,
         conn_health_checks=True,
     )
 }
