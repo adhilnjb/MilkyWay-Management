b78876c264cb08086f89fb5d824ac8b583044779
Adhil Najeeb <adhilnajeeb469@gmail.com>
2026-05-18 16:06:57 +0530

    Fix serverless routing entry point and binary drivers

--- milkdairy/wsgi.py
+++ milkdairy/wsgi.py
@@ -2,5 +2,9 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'milkdairy.settings')
+
+# Get the standard WSGI handler
 application = get_wsgi_application()
-app=application
+
+# ALIAS FOR VERCEL RUNTIME (Do not remove)
+app = application
